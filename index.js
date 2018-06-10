0c9b195689c2a79b448f8aca980186ba5148d89b
HanVincent <vincent.han.tw@gmail.com>
2018-06-10 22:06:12 +0800

    Feature: pick similar/better patterns

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ $(document).ready(() => {
                 'Content-Type': 'application/json'
             },
             url: "http://nlp-jedi.cs.nthu.edu.tw:1314/correct",
+            // url: "http://nlp-ultron.cs.nthu.edu.tw:1315/",
             contentType: 'application/json; charset=UTF-8',
             data: JSON.stringify({
                 'content': content
@@ -38,24 +39,25 @@ $(document).ready(() => {
 
     function showEdit(data) {
         console.log(data);
-        let { edit, suggestions=undefined } = data;
+        let { edit, suggestions = undefined } = data;
 
-        edit = edit.replace(rightPtn, "<span class='text-success'>$2</span>")
+        edit = edit.replace('\n', '<br>')
+            .replace(rightPtn, "<span class='text-success'>$2</span>")
             .replace(wrongPtn, "<span class='text-danger'>$2</span>")
             .replace(warningPtn, "<span class='text-warning'>$2</span>");
         editBlock.html(edit);
 
-        if(!suggestions) return ;
+        if (!suggestions) return;
 
         const sugList = suggestions.reduce((prev, curr, idx) => {
-            const {category, tk, bef, aft, ngram} = curr;
-            const textStyle = category === 'wrong'? 'text-danger':'text-warning';
+            const { category, tk, bef, aft, ngram } = curr;
+            const textStyle = category === 'wrong' ? 'text-danger' : 'text-warning';
             return prev + `<tr>
             <th scope="row" class="${textStyle}">${tk}</th>
             <td>${bef}</td><td>${aft}</td><td>${ngram}</td>
             </tr>`;
         }, '')
         sugTable.html(sugList);
-        
+
     }
 })
