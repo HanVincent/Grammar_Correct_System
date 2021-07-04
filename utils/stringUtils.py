46cf2497ddc6b8f954c50414df7d24df28837910
HanVincent <vincent.han.tw@gmail.com>
2021-07-04 14:18:08 +0800

    Add requirements.txt

--- utils/stringUtils.py
+++ utils/stringUtils.py
@@ -1,8 +1,4 @@
-import re
-import requests
 from itertools import product
-from bs4 import BeautifulSoup
-from readability import Document
 
 
 def edit_distance(s1, s2):
@@ -67,43 +63,3 @@ def duplicate_sent(sent):
     sents = [' '.join(sent) for sent in sents_compositions]
 
     return sents
-
-
-re_url = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    # domain...
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
-    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-
-def extract_main_content(url):
-
-    def _remove_all_attrs(soup):
-        for tag in soup.find_all(True):
-            tag.attrs = {}
-        return soup
-
-    def _remove_sometag(text):
-        soup = BeautifulSoup(text, 'html.parser')
-
-        # remove underline and hyperlink
-        invalid_tags = ['u', 'a', 'strong']
-        for tag in invalid_tags:
-            for match in soup.findAll(tag):
-                match.replaceWithChildren()
-        return str(soup)
-
-    if not re_url.match(url):
-        return url
-
-    response = requests.get(url)
-    # doc = _remove_sometag(Document(response.text).summary())
-    doc = Document(response.text).summary()
-    soup = BeautifulSoup(doc, features="lxml")
-    text = soup.get_text()
-
-    return text
